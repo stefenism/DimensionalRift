e37415a544baf39d00bbb2a40eaceaab59000df3
stefenmenzel <stefenmenzel@gmail.com>
2019-08-02 23:24:01 -0500

    initial commit

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager gameDaddy = null; //Static instance of GameManager allows it to be called from anywhere    
+    void Awake(){
+        //Check to see if there's a gameDaddy
+        if(gameDaddy == null){
+            //if there isn't. this will be our gameDaddy
+            gameDaddy = this;
+        }
+        //otherwise
+        else if (gameDaddy == this){
+            //KILL IT DEAD!
+            Destroy(gameObject);
+        }
+        //Keep our GameDaddy 4 E-V-E-R
+        DontDestroyOnLoad(gameObject);
+    }
+}
