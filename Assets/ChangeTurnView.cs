28c205632a91dc7957a69e610823793b9deb2a36
Zack Gilbertson <zack.gilbertson@gmail.com>
2019-08-04 12:37:08 -0500

    TurnViewer

---
+++ Assets/ChangeTurnView.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTurnView : MonoBehaviour
+{
+    
+
+    Animator animator;
+    int x;
+    void Start () {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x++;
+
+        if(x>100){
+            animator.SetBool("YourTurn",false);
+            animator.SetBool("EnemyTurn",true);
+        }
+    }
+}
